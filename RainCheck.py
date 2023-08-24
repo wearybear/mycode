cf70c2a1b3c9c1a145a8e416f0b9a67de0b6fd57
wearybear <j_moran_1980@hotmail.com>
2023-08-24 19:51:38 +0000

    version 0.11

--- RainCheck.py
+++ RainCheck.py
@@ -2,50 +2,16 @@
 
 import subprocess
 
-# Install necessary Python packages using pip3
+print("Installing necessary Python packages using pip3...")
 try:
     subprocess.check_call(['pip3', 'install', 'kivy'])
     print("Packages successfully installed.")
 except subprocess.CalledProcessError:
     print("Error installing packages.")
 
-import subprocess
-
-# Check the operating system to determine the appropriate package manager for libegl1-mesa
-def install_mesa_package():
-    try:
-        subprocess.check_call(['sudo', 'apt-get', 'install', 'libegl1-mesa'])
-        print("libegl1-mesa successfully installed.")
-    except subprocess.CalledProcessError:
-        print("Error installing libegl1-mesa.")
-import subprocess
-
-# Check the operating system to determine the appropriate package manager for xclip
-def install_mesa_package():
-    try:
-        subprocess.check_call(['sudo', 'apt-get', 'install', 'xclip'])
-        print("libegl1-mesa successfully installed.")
-    except subprocess.CalledProcessError:
-        print("Error installing libegl1-mesa.")
-
-# Run the installation function
-install_mesa_package()
-
-# Run the installation function
-install_mesa_package()
-
 import os
-import subprocess
-
-# Set environment variables for Kivy
-os.environ['KIVY_METRICS_DENSITY'] = '2'
-os.environ['KIVY_TEXT'] = 'sdl2'
-
 import kivy
-kivy.require('1.11.1')  # Replace with your Kivy version
 from kivy.config import Config
-Config.set('graphics', 'text', 'sdl2')
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -53,8 +19,33 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 import requests
 
+def install_mesa_package():
+    print("Installing libegl1-mesa package...")
+    try:
+        subprocess.check_call(['sudo', 'apt-get', 'install', 'libegl1-mesa'])
+        print("libegl1-mesa successfully installed.")
+    except subprocess.CalledProcessError:
+        print("Error installing libegl1-mesa.")
+
+def install_clipboard_package():
+    print("Installing clipboard package (xclip)...")
+    try:
+        subprocess.check_call(['sudo', 'apt-get', 'install', 'xclip'])
+        print("xclip successfully installed.")
+    except subprocess.CalledProcessError:
+        print("Error installing xclip.")
+
+print("Setting environment variables for Kivy...")
+os.environ['KIVY_METRICS_DENSITY'] = '2'
+os.environ['KIVY_TEXT'] = 'sdl2'
+
+print("Importing Kivy...")
+kivy.require('1.11.1')
+Config.set('graphics', 'text', 'sdl2')
+
 class WeatherApp(App):
     def build(self):
+        print("Building the app interface...")
         self.layout = BoxLayout(orientation='vertical')
         self.city_input = TextInput(hint_text='Enter city')
         self.weather_label = Label(text='Weather will be displayed here')
@@ -67,8 +58,9 @@ class WeatherApp(App):
         return self.layout
     
     def fetch_weather(self, instance):
+        print("Fetching weather data...")
         city = self.city_input.text
-        api_key = 'API key here'
+        api_key = 'Instert API Key here'
         base_url = f'http://dataservice.accuweather.com/currentconditions/v1/{city}?apikey={api_key}'
         
         response = requests.get(base_url)
@@ -82,5 +74,6 @@ class WeatherApp(App):
             self.weather_label.text = 'Weather data not available.'
 
 if __name__ == '__main__':
+    print("Starting the WeatherApp...")
     WeatherApp().run()
 
