c2f3344debeb95beb0b0c211798f561007e053d2
wearybear <j_moran_1980@hotmail.com>
2023-08-24 22:21:42 +0000

    fixing code

--- RainCheck.py
+++ RainCheck.py
@@ -1,79 +1,63 @@
 #!/usr/bin/python3
 
-import subprocess
-
-print("Installing necessary Python packages using pip3...")
-try:
-    subprocess.check_call(['pip3', 'install', 'kivy'])
-    print("Packages successfully installed.")
-except subprocess.CalledProcessError:
-    print("Error installing packages.")
-
-import os
-import kivy
-from kivy.config import Config
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 import requests
-
-def install_mesa_package():
-    print("Installing libegl1-mesa package...")
-    try:
-        subprocess.check_call(['sudo', 'apt-get', 'install', 'libegl1-mesa'])
-        print("libegl1-mesa successfully installed.")
-    except subprocess.CalledProcessError:
-        print("Error installing libegl1-mesa.")
-
-def install_clipboard_package():
-    print("Installing clipboard package (xclip)...")
-    try:
-        subprocess.check_call(['sudo', 'apt-get', 'install', 'xclip'])
-        print("xclip successfully installed.")
-    except subprocess.CalledProcessError:
-        print("Error installing xclip.")
-
-print("Setting environment variables for Kivy...")
-os.environ['KIVY_METRICS_DENSITY'] = '2'
-os.environ['KIVY_TEXT'] = 'sdl2'
-
-print("Importing Kivy...")
-kivy.require('1.11.1')
-Config.set('graphics', 'text', 'sdl2')
 
 class WeatherApp(App):
     def build(self):
-        print("Building the app interface...")
-        self.layout = BoxLayout(orientation='vertical')
-        self.city_input = TextInput(hint_text='Enter city')
-        self.weather_label = Label(text='Weather will be displayed here')
-        self.fetch_button = Button(text='Fetch Weather', on_press=self.fetch_weather)
+        self.title = 'RainCheck'
+        self.orientation = 'vertical'
         
-        self.layout.add_widget(self.city_input)
-        self.layout.add_widget(self.fetch_button)
-        self.layout.add_widget(self.weather_label)
+        self.city_input = TextInput(hint_text='Enter City, State')
+        self.fetch_button = Button(text='Fetch Weather')
+        self.weather_label = Label(text='', halign='center')
         
-        return self.layout
+        self.fetch_button.bind(on_press=self.fetch_weather)
+        
+        self.add_widget(self.city_input)
+        self.add_widget(self.fetch_button)
+        self.add_widget(self.weather_label)
+        
+        return self
     
     def fetch_weather(self, instance):
-        print("Fetching weather data...")
-        city = self.city_input.text
-        api_key = 'Instert API Key here'
-        base_url = f'http://dataservice.accuweather.com/currentconditions/v1/{city}?apikey={api_key}'
+        city_state = self.city_input.text
+        if not city_state:
+            self.weather_label.text = 'Please enter a city and state'
+            return
         
-        response = requests.get(base_url)
-        data = response.json()
+        # Step 1: Call API to get key code for the city
+        api_key = "Insert API Key Here"
+        location_url = f"http://dataservice.accuweather.com/locations/v1/cities/search"
+        params = {
+            "apikey": api_key,
+            "q": city_state
+        }
+        response = requests.get(location_url, params=params)
+        location_data = response.json()
         
-        if data:
-            weather_text = data[0]['WeatherText']
-            temperature = data[0]['Temperature']['Metric']['Value']
-            self.weather_label.text = f'Weather: {weather_text}\nTemperature: {temperature}°C'
-        else:
-            self.weather_label.text = 'Weather data not available.'
+        if not location_data:
+            self.weather_label.text = 'City not found'
+            return
+        
+        city_key = location_data[0]['Key']
+        
+        # Step 2: Use key code to fetch weather data
+        weather_url = f"http://dataservice.accuweather.com/currentconditions/v1/{city_key}"
+        params = {
+            "apikey": api_key
+        }
+        response = requests.get(weather_url, params=params)
+        weather_data = response.json()
+        
+        if not weather_data:
+            self.weather_label.text = 'Weather data not available'
+            return
+        
+        weather_text = weather_data[0]['WeatherText']
+        temperature = weather_data[0]['Temperature']['Metric']['Value']
+        self.weather_label.text = f'Weather: {weather_text}\nTemperature: {temperature}°C'
 
 if __name__ == '__main__':
-    print("Starting the WeatherApp...")
     WeatherApp().run()
-
